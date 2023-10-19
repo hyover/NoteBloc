878d535c99e68b959c43105b0a33a74cb6ed80fe
hyover <vf.hyover@gmail.com>
2023-10-19 08:42:56 +0200

    Ajoutez des fichiers projet.

---
+++ NoteBloc/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace NoteBloc
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentTextBox.Clear();
+        }
+
+        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                CurrentTextBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "Sans titre.txt";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, CurrentTextBox.Text);
+            }
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void CutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentTextBox.Cut();
+        }
+
+        private void CopyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentTextBox.Copy();
+        }
+
+        private void PasteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentTextBox.Paste();
+        }
+
+        private void NewTabMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem newTab = new TabItem
+            {
+                Header = "Sans titre",
+                Content = new TextBox
+                {
+                    AcceptsReturn = true,
+                    AcceptsTab = true,
+                    VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+                    HorizontalScrollBarVisibility = ScrollBarVisibility.Auto,
+                    TextWrapping = TextWrapping.Wrap
+                }
+            };
+
+            tabControlNotes.Items.Add(newTab);
+            tabControlNotes.SelectedItem = newTab;
+        }
+
+        private void CloseTabMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem currentTab = tabControlNotes.SelectedItem as TabItem;
+            if (NoteHasUnsavedChanges(currentTab))
+            {
+                SaveChangesDialog dialog = new SaveChangesDialog(currentTab.Header.ToString());
+                dialog.ShowDialog();
+
+                switch (dialog.Result)
+                {
+                    case CustomDialogResult.Save:
+                        // Enregistrez la note ici.
+                        break;
+                    case CustomDialogResult.DontSave:
+                        tabControlNotes.Items.Remove(currentTab);
+                        if (tabControlNotes.Items.Count == 0)
+                            this.Close();  // ferme la fenêtre si c'est le dernier onglet
+                        break;
+                    case CustomDialogResult.Cancel:
+                        // Ne faites rien.
+                        break;
+                }
+            }
+            else
+            {
+                tabControlNotes.Items.Remove(currentTab);
+                if (tabControlNotes.Items.Count == 0)
+                    this.Close();  // ferme la fenêtre si c'est le dernier onglet
+            }
+        }
+
+        private bool NoteHasUnsavedChanges(TabItem tabItem)
+        {
+            // Cette méthode vérifie si la note a des modifications non enregistrées.
+            // Pour l'instant, c'est une simple vérification pour voir si le contenu est vide. 
+            // Vous pouvez améliorer cette vérification en fonction de vos besoins.
+            var textBox = tabItem.Content as TextBox;
+            return !string.IsNullOrEmpty(textBox?.Text);
+        }
+
+        private TextBox CurrentTextBox => ((tabControlNotes.SelectedItem as TabItem)?.Content as TextBox);
+
+        private void tabControlNotes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NoteBloc/SaveChangesDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace NoteBloc;
+public partial class SaveChangesDialog : Window
+{
+    public CustomDialogResult Result { get; private set; }
+
+    public SaveChangesDialog(string noteName)
+    {
+        InitializeComponent();
+        txtMessage.Text = $"Voulez-vous enregistrer les modifications de {noteName}?";
+    }
+
+    private void SaveButton_Click(object sender, RoutedEventArgs e)
+    {
+        Result = CustomDialogResult.Save;
+        this.Close();
+    }
+
+    private void DontSaveButton_Click(object sender, RoutedEventArgs e)
+    {
+        Result = CustomDialogResult.DontSave;
+        this.Close();
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        Result = CustomDialogResult.Cancel;
+        this.Close();
+    }
+}
+
+public enum CustomDialogResult
+{
+    Save,
+    DontSave,
+    Cancel
+}
+
+
